ae58774765f48d16266a00227ec639626bd2989b
adamkaj <adam2000buzas@gmail.com>
2023-06-16 19:28:11 +0200

    Listázás megvan

--- main/src/main/java/com/inerview/main/controller/StudentController.java
+++ main/src/main/java/com/inerview/main/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.inerview.main.controller;
 
+import com.inerview.main.model.Student;
 import com.inerview.main.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -13,8 +16,9 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping("/students")
-    public String getStudents(){
-        return "Hello";
+    public List<Student> getStudents(){
+        List<Student> lista = studentService.getAllStudent();
+        return lista;
     }
 
     @PutMapping("/update")
